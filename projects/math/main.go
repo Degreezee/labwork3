555e5b87a5622f2ecb40371a6d12fbabf8e3b3a3
Degreezee <ivanragulin001@gmail.com>
2024-10-05 11:07:18 +0300

    Done!

--- projects/math/main.go
+++ projects/math/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+var p int = 6
+var v int = 6
+var k int = 1296
+
+func M() float64 {
+	return float64(p * v)
+}
+
+func W() float64 {
+	return math.Sqrt((float64(k) / M()))
+}
+
+func T() float64 {
+	return 6 / W()
+}
 
 func main() {
-	fmt.Println("Hello, world!!!")
+	fmt.Println(T())
 }
